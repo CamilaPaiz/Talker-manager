8f02795cc5d7e3b53a77e213eafba592280977de
CamilaPaiz <camilapaiz@hotmail.com>
2022-12-14 15:57:16 -0300

    iniciando projeto talker-manager

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+const fs = require('fs').promises;
+const path = require('path');
+
+const JSON_PATH = path.resolve(__dirname, 'talker.json');
+
 const app = express();
 app.use(express.json());
 
@@ -14,3 +19,30 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+// requisito 1 -get /talker
+
+const readFile = async (pathh) => {
+  try {
+    const data = await fs.readFile(pathh, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    return [];
+  }
+};
+
+const getAllTalker = async () => {
+  const talker = await readFile(JSON_PATH);
+  return talker;
+};
+
+app.get('/talker', async (_req, res) => {
+const talkerInfo = await getAllTalker();
+return res.status(200).json(talkerInfo);
+});
+
+// requisito 2 /talker/:id
+
+module.exports = {
+  readFile,
+};
