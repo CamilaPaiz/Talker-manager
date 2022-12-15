93fd429e332d210346b418065f6e7dac26afcdfb
CamilaPaiz <camilapaiz@hotmail.com>
2022-12-15 19:41:10 -0300

    requisito5

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ const {
   validateEmail, 
   validateEmailRegex, 
   validatePassword, 
-  validatePasswordLength } = require('./middleware/validate');
+  validatePasswordLength, 
+  validateAge,
+  validateName,
+  validateTalkRate,
+  validateWatchedAt,
+  validateAutorization,
+  validateTalk, 
+} = require('./middleware/validate');
 
 const JSON_PATH = path.resolve(__dirname, 'talker.json');
 
@@ -88,7 +95,93 @@ validatePasswordLength,
 // requisito 4 /login
 // realizado middleware e chamdo acima
 
+// requisito 5 post /talker
+
+app.post('/talker',
+validateAutorization,
+validateAge, 
+validateName, 
+validateTalk,
+validateTalkRate,
+validateWatchedAt,
+
+async (req, res) => {
+  const talker = await readFile(JSON_PATH);
+  const { name, age, talk } = req.body;
+ const newTalker = {
+  id: talker.length + 1,
+  name,
+  age,
+  talk,
+ };
+  talker.push(newTalker);
+  /* const addedTalker = talker.filter((talkers) => talkers.id === id); */
+  try {
+    await fs.writeFile(JSON_PATH, JSON.stringify(talker, null, 2));
+  } catch (err) {
+    return null;
+  } 
+  const addedTalker = talker[talker.length - 1];
+   return res.status(201).json(addedTalker);
+});
+
+// requisito 6 put /talker/:id
+ /*  app.put('/talker/:id',
+validateTalk,
+validateAutorization,
+validateName,
+validateAge,
+validateWatchedAt,
+validateTalkRate,
+
+ async (req, res) => {
+  const { id } = req.params;
+  const { name, age, rate, watchedAt } = req.body;
+  const talker = await getAllTalker();
+
+  const newTalker = talker.find(
+      (talkers) => talkers.id === Number(id),
+);
+  
+ newTalker.talk.rate = rate;
+ newTalker.name = name;
+ newTalker.age = age;
+ newTalker.talk.watchedAt = watchedAt;
+
+  return res.status(200).json(talker);
+});
+ */
+// requisito 7 delete /talker/:id
+
+   /* const deleteTalker = async (id) => {
+  const talker = await getAllTalker();
+  const talkerAfterDelete = talker.filter((talkers) => talkers.id !== id);
+  await writeFile(talkerAfterDelete, JSON_PATH);
+  return talkerAfterDelete;
+};
+
+app.delete('talker/:id', validateAutorization, async (req, res) => {
+  const { id } = req.params;
+   await deleteTalker(id);
+  return res.status(204).json();
+});   */
+ 
+// requisito 8 get /talker/search?q=searchTerm
+
+  /* app.get('/talker', validateAutorization, async (req, res) => {
+const { name } = req.query;
+const talker = await getAllTalker();
+const queryTalker = talker.filter((talkers) => talkers.name === name);
+if (!queryTalker) {
+  return res.status(200).json(talker);
+}
+if (queryTalker !== talker) {
+  return res.status(200).json([]);
+}
+return res.status(200).json(queryTalker);
+});  */
 module.exports = {
   readFile,
   getId,
+  generateToken,
 };
